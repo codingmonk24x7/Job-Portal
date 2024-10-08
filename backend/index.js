8caaadcc9be38fecebf9181f5b09e1cd06f351de
Ramansh <ramanshag001@gmail.com>
2024-10-08 21:04:58 +0530

    Pushing the app for production

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import userRoute from "./routes/userRoute.js";
 import companyRoute from "./routes/companyRoute.js";
 import jobRoute from "./routes/jobRoute.js";
 import applicationRoute from "./routes/applicationRoute.js";
+import path from "path";
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ dotenv.config();
 connectDB();
 const PORT = process.env.PORT || 4000;
 const app = express();
-
+const _dirname = path.resolve();
 
 // middleware
 app.use(express.json());
@@ -35,7 +36,10 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
-
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_,res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+})
 app.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`);
 });
